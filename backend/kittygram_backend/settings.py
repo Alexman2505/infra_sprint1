c5abaf96b240458df4faad873ed13e19047514d4
alexman2505_server <alexman2505@gmail.com>
2023-07-15 16:58:12 +0000

    отправляю на ревью, debug в сеттингах и ридми поправил

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,9 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'пустая строка, чтобы хоть как-то проект запустился')
 
-DEBUG = False
-
-#DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
